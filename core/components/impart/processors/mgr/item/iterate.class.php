89c8f4b163a43bff6c93d325259a2e34583f8056
Dudarev Ilia <dudareviv@gmail.com>
2017-01-28 13:57:37 +0400

    Изменил логику итерации, при размещении ресурсов: каждый новый дочерний ресурс будет получать menu_index = max(menu_index) +1. Максимальный индекс ищется внутри дочерних записей родителя.

--- core/components/impart/processors/mgr/item/iterate.class.php
+++ core/components/impart/processors/mgr/item/iterate.class.php
@@ -1,23 +1,30 @@
 <?php
-
 /**
  * Send an Queue
  */
-class impArtItemIterateProcessor extends modProcessor
-{
+class impArtItemIterateProcessor extends modProcessor {
     public $objectType = 'impArticle';
     public $classKey = 'impArticle';
 
-    public function process()
-    {
+    public function process() {
         $c = $this->modx->newQuery($this->classKey, array(
             'imported' => 0,
             'alias_dublicate' => 0
         ));
-        $c->limit(10);
+        $c->limit(100);
+        $c->sortby('id', 'ASC');
+
         $articles = $this->modx->getCollection($this->classKey, $c);
         foreach ($articles as $article) {
-            $response = $this->modx->runProcessor('resource/create', $article->toArray());
+            $data = $article->toArray();
+
+            $prefix = $this->modx->config['table_prefix'];
+            $parent = $data['parent'];
+
+            $query = $this->modx->query("SELECT MAX(menuindex) FROM {$prefix}site_content WHERE parent = {$parent}");
+            $data['menuindex'] = intval($this->modx->getValue($query)) + 1;
+
+            $response = $this->modx->runProcessor('resource/create', $data);
             if ($response->isError()) {
                 return $response->response;
             }
